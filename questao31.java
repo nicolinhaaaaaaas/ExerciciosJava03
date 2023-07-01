f679840bf0b8b6450a57af0c6d713d1efe5a0ce4
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-01 17:53:34 -0300

    Update questao31.java
    agora sim ta certo

--- questao31.java
+++ questao31.java
@@ -19,32 +19,32 @@ public class Elevador {
 			opcao2 = scanner.nextInt();
 			
 			switch(opcao2) {
-			case 1:
-				construtor();
-			break;
-			
-			case 2:
-				entra();
-			break;
-			
-			case 3:
-				sai();
-			break;
-			
-			case 4:
-				sobe();
-			break;
-			
-			case 5:
-				desce();
-			break;
-			
-			case 0:
-				System.out.println("Encerrando o programa...");
-				return;
-			default:
-				System.out.println("Opcao invalida.");
-			break;
+				case 1:
+					construtor();
+				break;
+				
+				case 2:
+					entra();
+				break;
+				
+				case 3:
+					sai();
+				break;
+				
+				case 4:
+					sobe();
+				break;
+				
+				case 5:
+					desce();
+				break;
+				
+				case 0:
+					System.out.println("Encerrando o programa...");
+					return;
+				default:
+					System.out.println("Opcao invalida.");
+				break;
 			}
 		}
 	}
@@ -53,20 +53,20 @@ public class Elevador {
 			
 		System.out.println("Voce escolheu o exercicio sobre o ELEVADOR, o que deseja fazer?");
 		System.out.print("(1)- Construtor ; (2)- Entrar ; (3)- Sair ; (4)- Subir ; (5)- Descer ; (0)-SAIR ");
+		System.out.println("");
 	}
-
-  //Tô tendo dificuldade em fazer os métodos funcionarem, mas dps eu arrumo
 	
 	void construtor() {
 		System.out.println("O elevador tem a capacidade de suportar "+capacidade+" pessoas.");
 		System.out.println("O predio tem o total de "+totalAndar+" andares");
+		System.out.println("");
 	}
 	
 	void entra() {
 		System.out.println("O elevador possui "+presentes+" pessoas presentes.");
 		System.out.print("Deseja que uma pessoa ENTRE no elevador? ");
 		escolha = scanner.next();
-		while(escolha != "nao") {
+		if(escolha != "nao") {
 			if(presentes <= capacidade) {
 				System.out.println("Mais uma pessoa entrou no elevador.");
 				presentes++;
@@ -74,13 +74,14 @@ public class Elevador {
 				System.out.println("O elevador esta lotado.");
 			}
 		}
+		System.out.println("");
 	}
 	
 	void sai() {
 		System.out.println("O elevador possui "+presentes+" pessoas presentes.");
 		System.out.print("Deseja que uma pessoa SAIA do elevador? ");
 		escolha = scanner.next();
-		while(escolha != "nao") {
+		if(escolha != "nao") {
 			if(presentes != 0) {
 				System.out.println("Uma pessoa saiu do elevador.");
 				presentes--;
@@ -88,13 +89,14 @@ public class Elevador {
 				System.out.println("Nao tem ninguem no elevador.");
 			}
 		}
+		System.out.println("");
 	}
 	
 	void sobe() {
 		System.out.println("O elevador esta no andar "+andar);
 		System.out.print("Deseja SUBIR um andar? ");
 		escolha = scanner.next();
-		while(escolha != "nao") {
+		if(escolha != "nao") {
 			if(andar < totalAndar) {
 				andar++;
 				System.out.println("Voce subiu para o andar "+andar);
@@ -102,13 +104,14 @@ public class Elevador {
 				System.out.println("Voce esta no ultimo andar, nao eh possivel subir mais.");
 			}
 		}
+		System.out.println("");
 	}
 	
 	void desce() {
 		System.out.println("O elevador esta no andar "+andar);
 		System.out.print("Deseja DESCER um andar? ");
 		escolha = scanner.next();
-		while(escolha != "nao") {
+		if(escolha != "nao") {
 			if(andar > 0) {
 				andar--;
 				System.out.println("Voce desceu para o andar "+andar);
@@ -116,5 +119,6 @@ public class Elevador {
 				System.out.println("Voce esta no terreo, nao eh possivel descer mais.");
 			}
 		}
+		System.out.println("");
 	}
 }
